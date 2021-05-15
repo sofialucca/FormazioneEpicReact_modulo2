06ad20b3d19cd4d945bb293e51031ee770047f9f
Sofia <sofy99@fastwebnet.it>
2021-05-15 17:56:09 +0200

    Problemi con js da risolvere

--- on-boarding/index.js
+++ on-boarding/index.js
@@ -1,16 +1,45 @@
 use strict;
 
-function traslare(idSpostamento){
-    // let elSpostare = document.getElementsByClassName(idSpostamento);
-    // elSpostare1 = document.getElementById(idSpostamento);
-    // elSpostare1.style.transform="translateX(-100%)"
-    // elSpostare1.style.translation="transform, 400ms, linear";
-    elSpostare = document.querySelectorAll("idSpostamento");
-    console.log(elSpostare);
-    elSpostare.forEach(element => {
-        element.style.transform = "translateX(-100%)";
-        element.style.trasition =  "transform, 400ms, linear";
-    });
-    // elSpostare.style.
-    // elSpostare.style.translation=
+var nClick=0;
+function traslare(idSpostamento1, idSpostamento2, idStepper, idContButton, idButton){
+    if(nClick<=2){
+        let elSpostare1 = document.querySelectorAll(idSpostamento1);
+        nClick+=1;
+        let transform = `translateX(-${nClick*100}%)`
+        elSpostare1.forEach(element => {
+            element.style.transform = transform;
+
+        });
+
+        let elSpostare2 = document.querySelectorAll(idSpostamento2);
+        elSpostare2.forEach(element => {
+            element.style.transform =transform;
+        });
+        
+
+        if(nClick>2){
+            elSpostare1[nClick].style.marginTop = "5%";
+            let elSpostare3 = document.querySelector(idStepper)
+            elSpostare3.style.transform =transform;
+            let bottone = document.querySelector(idContButton);
+            bottone.style.transform = transform;
+
+        }else{
+            let elSpostare3 = document.querySelectorAll("span");
+            elSpostare3.forEach(element =>{
+                element.style.color= '#FFFFFF';
+            })
+
+            elSpostare3[nClick].style.color= '#00A09A';
+
+            if(nClick==2){
+            cambioBottone("FINE",idButton)
+            }
+        }
+
+    }
+}
+function cambioBottone(testo,idButton){
+    let bottone = document.querySelector(idButton);
+    bottone.value = testo;
 }
